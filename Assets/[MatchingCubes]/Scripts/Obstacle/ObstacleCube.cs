13e9b860e3ca14b9b3722c1038cb927aff334ef4
Mert <41839620+mertvnl@users.noreply.github.com>
2022-11-01 02:14:40 +0300

    Fix: Obstacle dispose fix

--- Assets/[MatchingCubes]/Scripts/Obstacle/ObstacleCube.cs
+++ Assets/[MatchingCubes]/Scripts/Obstacle/ObstacleCube.cs
@@ -7,8 +7,22 @@ public class ObstacleCube : ObstacleBase
     private Rigidbody rb;
     public Rigidbody Rb { get { return rb == null ? rb = GetComponent<Rigidbody>() : rb; } }
 
+    private bool isDisposed;
+
     public override void Dispose()
     {
+        if (isDisposed) return;
+
+        isDisposed = true;
+
+        var colliders = Physics.OverlapSphere(transform.position, 2);
+
+        foreach (var col in colliders)
+        {
+            if (col.TryGetComponent(out IObstacle obstacle))
+                obstacle.Dispose();
+        }
+
         Rb.isKinematic = false;
         Rb.AddForce(new Vector3(Random.Range(-1, 2), 1, Random.Range(-1, 2)) * 100);
     }
